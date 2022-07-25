d794c9450b0402e27b5a92073cdd3439e2de5d05
silversray <melan.sekoua@ensae-paristech.fr>
2022-07-25 22:50:50 +0000

    design the draft of townhall project with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { FooterComponent } from './components/partials/footer/footer.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { HeaderPageComponent } from './components/partials/header-page/header-page.component';
+import { QuickViewModalComponent } from './components/partials/modal/quick-view-modal/quick-view-modal.component';
+import { AddToCartModalComponent } from './components/partials/modal/add-to-cart-modal/add-to-cart-modal.component';
+import { DeleteProductComponent } from './components/partials/modal/delete-product/delete-product.component';
+import { SingleProductComponent } from './townHall/single-product/single-product.component';
+import { CartComponent } from './townHall/cart/cart.component';
+import { AddProductComponent } from './townHall/add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HeaderPageComponent,
+    QuickViewModalComponent,
+    AddToCartModalComponent,
+    DeleteProductComponent,
+    SingleProductComponent,
+    CartComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/partials/modal/quick-view-modal/quick-view-modal.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuickViewModalComponent } from './quick-view-modal.component';
+
+describe('QuickViewModalComponent', () => {
+  let component: QuickViewModalComponent;
+  let fixture: ComponentFixture<QuickViewModalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ QuickViewModalComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(QuickViewModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
